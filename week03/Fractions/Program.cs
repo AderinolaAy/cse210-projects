31135d92f9e91d94407ec1e002d5aecb4318fce3
Aderinola <aderinolaayoyemi@gmail.com>
2026-01-18 21:57:15 +0100

    fraction exercise complete

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -6,4 +6,72 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
     }
+    // Private attributes
+    private int _top;
+    private int _bottom;
+
+    // Default constructor: initializes to 1/1
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor with one parameter: top, bottom defaults to 1
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    // Constructor with two parameters: top and bottom
+    public Fraction(int top, int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getter for top
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    // Setter for top
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    // Getter for bottom
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    // Setter for bottom
+    public void SetBottom(int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _bottom = bottom;
+    }
+
+    // Returns the fraction as a string (e.g., "3/4")
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    // Returns the decimal value of the fraction (e.g., 0.75)
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
 }
